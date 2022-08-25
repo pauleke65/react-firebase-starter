fe0e04fba9a43aae2b773c53839be0e46a6eded7
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2022-08-25 16:23:59 -0400

    update setup script to automate firebase setup

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,8 +1,13 @@
 const cp = require('child_process');
 const path = require('path');
+const readline = require('readline');
 
 const mainPath = path.resolve(__dirname, '../');
 const functionPath = path.resolve(mainPath, 'functions');
+
+const logWithFormat = (string) => {
+  console.log('\x1b[34m%s\x1b[0m', `\n${string}\n`);
+};
 
 const runCmd = (cmd, params, cwd = null) => {
   const { status } = cp.spawnSync(cmd, params, {
@@ -13,14 +18,58 @@ const runCmd = (cmd, params, cwd = null) => {
   if (status !== 0) process.exit(status);
 };
 
-(() => {
-  console.log('Installing main dependencies...');
+const escapeSpecialChars = (string) => {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
+
+const getUserInput = (prompt) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise(resolve => rl.question(prompt, userInput => {
+    rl.close();
+    const sanitizedInput = escapeSpecialChars(userInput);
+    resolve(sanitizedInput);
+  }))
+};
+
+const getKeypress = () => {
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  return new Promise(resolve => process.stdin.once('data', () => {
+    process.stdin.setRawMode(false);
+    resolve();
+  }))
+};
+
+(async () => {
+  logWithFormat('To use React and Firebase Starter, you will need access to a Firebase project.\nIf you don\'t have a project yet, head to https://console.firebase.google.com, click Add project, then enable Firestore Database and Authentication via Google.\nWhen you are ready, press any key to continue. ');
+
+  await getKeypress();
+
+  logWithFormat('Installing main dependencies...');
   runCmd('npm', ['ci'], mainPath);
-  console.log('Installing functions dependencies...');
+  logWithFormat('Installing functions dependencies...');
   runCmd('npm', ['ci'], functionPath);
-  console.log('Dependencies installed.');
-  console.log('Firebase login...');
+  logWithFormat('Dependencies installed!');
+  logWithFormat('Firebase login...');
   runCmd('firebase', ['login']);
-  console.log('Firebase init...');
+  logWithFormat('Getting your Firebase projects...');
+  runCmd('firebase', ['projects:list']);
+
+  const projectId = await getUserInput("Enter the Project ID of the project you want to use: ");
+  
+  runCmd('firebase', ['use', projectId]);
+  logWithFormat('Saving your project config to a local .env file...');
+  runCmd('node', ['scripts/configTool.js']);
+  logWithFormat("You are about to initialize a local Firebase directory.\nThis project is set up to use Firestore, Functions, and Emulators. Make sure you choose these options when prompted by Firebase CLI.\nTo take advantage of this Starter's features, choose not to overwrite existing files when asked by the CLI.\nPress any key to continue. ");
+
+  await getKeypress();
+
+  logWithFormat('Initializing Firebase directory for your project...');
   runCmd('firebase', ['init']);
+
+  process.exit();
 })();
